11759ad40713cfd757fe137865ee71287747ad7a
Pongo <echotech8310@gmail.com>
2026-07-27 14:41:10 +0700

    style(ui): remove quote text from auth left section

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -9,13 +9,6 @@
                 <span class="m-0 font-weight-bold h3">Admin</span>
             </div>
             
-            <div class="login-left-content mt-auto">
-                <div class="quote-text">
-                    "Sistem lengkap yang<br>saya dan tim butuhkan."
-                </div>
-                <div class="quote-author">Karen Yue</div>
-                <div class="quote-title">Direktur Teknologi Pemasaran Digital</div>
-            </div>
         </div>
         
         <!-- Right Side -->
