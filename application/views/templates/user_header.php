43f8f3b3aacd4254a2ae7ba691a44b61fe684809
Pongo <echotech8310@gmail.com>
2026-07-27 15:39:54 +0700

    fix(ui): restore modal-lg on delegate permission modal in merchant management to fix cramped layout

--- application/views/templates/user_header.php
+++ application/views/templates/user_header.php
@@ -29,7 +29,7 @@
 	<!-- Custom styles for this template-->
 	<link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
 	<!-- Premium UI/UX Override -->
-	<!-- <link href="<?= base_url('assets/'); ?>css/premium-theme.css" rel="stylesheet"> -->
+	<link href="<?= base_url('assets/'); ?>css/premium-theme.css" rel="stylesheet">
 
 	<!-- Select2 CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
